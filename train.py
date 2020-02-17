1dd24b86b9eb782682aa90fb143c0d7637a78aac
Yana <n.y.shpot@ucu.edu.ua>
2020-02-17 20:24:39 +0200

    Update train.py

--- train.py
+++ train.py
@@ -77,6 +77,8 @@ class Trainer(object):
 			self.metric_counter.add_metrics(curr_psnr, curr_ssim)
 			tq.set_postfix(loss=self.metric_counter.loss_message())
 			i += 1
+			if i == 3:
+				self.metric_counter.images_to_tensorboard([inputs[0], targets[0], outputs[0]], epoch)
 		tq.close()
 		self.metric_counter.write_to_tensorboard(epoch)
 
@@ -97,8 +99,8 @@ class Trainer(object):
 			self.metric_counter.add_losses(loss_G.item(), loss_content.item(), loss_vgg.item())
 			curr_psnr, curr_ssim = self.model.get_acc(outputs, targets)
 			self.metric_counter.add_metrics(curr_psnr, curr_ssim)
-			if i == 3:
-				self.metric_counter.images_to_tensorboard([inputs[0], targets[0], outputs[0]], epoch)
+# 			if i == 3:
+# 				self.metric_counter.images_to_tensorboard([inputs[0], targets[0], outputs[0]], epoch)
 		tq.close()
 		self.metric_counter.write_to_tensorboard(epoch, validation=True)
 
